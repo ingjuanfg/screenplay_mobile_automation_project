40ba1a2632447099dff8a4b41525b780c6a2bba3
juferz <juan.isfg@gmail.com>
2022-07-12 21:37:34 -0500

    Se ajustan archivos .properties para correcta ejecución

--- src/main/java/com/ingjuanfg/userinterface/LoginPage.java
+++ src/main/java/com/ingjuanfg/userinterface/LoginPage.java
@@ -8,11 +8,11 @@ import static net.serenitybdd.screenplay.targets.Target.the;
 public class LoginPage {
     public static final Target TXT_USER = the("Input Username")
             .locatedForAndroid(MobileBy.AccessibilityId("test-Username"))
-            .locatedForIOS(MobileBy.AccessibilityId("test-Username"));
+            .locatedForIOS(MobileBy.iOSNsPredicateString("name == 'test-Username'"));
 
     public static final Target TXT_PASSWORD = the("Input Password")
             .locatedForAndroid(MobileBy.AccessibilityId("test-Password"))
-            .locatedForIOS(MobileBy.AccessibilityId("test-Password"));
+            .locatedForIOS(MobileBy.iOSClassChain("**/XCUIElementTypeSecureTextField[`name == 'test-Password'`]"));
 
     public static final Target BTN_LOGIN = the("Button Login")
             .locatedForAndroid(MobileBy.xpath("//android.view.ViewGroup[@content-desc='test-LOGIN']"))
